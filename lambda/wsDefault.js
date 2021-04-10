e95a3d7afa8c753e16c9d80ec6ca447fd1019b3f
AmirKAT <amir-katal@hotmail.com>
2021-04-10 15:45:45 +0100

    added wsDefault

--- lambda/wsDefault.js
+++ lambda/wsDefault.js
@@ -1,10 +1,28 @@
-exports.handler = async (event) => {
-    console.log("EVENT:" + JSON.stringify(event));
+let AWS = require("aws-sdk");
 
-    //Build and return response with error message
-    const response = {
-        statusCode: 500,
-        body: JSON.stringify('ERROR. Message not recognized.'),
-    };
-    return response;
+exports.handler = async (event) => {
+    try {
+        console.log("EVENT:" + JSON.stringify(event));
+        const { domainName, stage, connectionId } = event.requestContext;
+        const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+            endpoint: domainName + '/' + stage
+        });
+        let apiMsg = {
+            ConnectionId: connectionId,
+            Data: JSON.stringify("Path not valid")
+        };
+
+        await apigwManagementApi.postToConnection(apiMsg).promise();
+
+        //Build and return response with error message
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify('ERROR. Message not recognized.'),
+        };
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
 };
+
+
